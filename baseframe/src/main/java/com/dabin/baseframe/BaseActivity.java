1cfe47acda488451244403cef48f19e3f57f4e7b
DabinZhang <zdb1988asdf@163.com>
2020-08-11 15:49:45 +0800

    新增工具

---
+++ baseframe/src/main/java/com/dabin/baseframe/BaseActivity.java
@@ -0,0 +1,34 @@
+package com.dabin.baseframe;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Created by Dabin on 2020/8/11 0011.
+ */
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initView();
+        initDate();
+        initEvent();
+
+    }
+
+    private void initEvent() {
+    }
+
+    private void initDate() {
+    }
+
+    private void initView() {
+    }
+
+
+
+}
+
--- utils/src/main/java/com/dabin/utils/TimeUtil.java
+++ utils/src/main/java/com/dabin/utils/TimeUtil.java
@@ -70,6 +70,4 @@ public class TimeUtil {
         return res;
     }
 
-
-
 }
